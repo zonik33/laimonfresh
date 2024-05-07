dbd2c48dce8712f28ea58baf3fd4487f55ca9b09
zonik33 <thezonikk@gmail.com>
2024-05-07 14:58:44 +0300

    mob 100

--- src/components/lk-profile/SliderCards.js
+++ src/components/lk-profile/SliderCards.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import lfmint from "../../image/img_29.png";
 import lfmintfull from "../../image/img_31.png";
+import {debounce} from "lodash";
 
 export default function SliderCards() {
     const [isFullImageOpen, setIsFullImageOpen] = useState(false);
@@ -38,6 +39,29 @@ export default function SliderCards() {
         slidesToShow: 4,
         speed: 500
     };
+    const settingsMobile = {
+        className: 'center',
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5555,
+        pauseOnHover: true
+    };
+    const [sliderSettings, setSliderSettings] = useState(settings);
+    const handleResize = debounce(() => {
+        if (window.innerWidth <= 767) {
+            setSliderSettings(settingsMobile);
+        } else {
+            setSliderSettings(settings);
+        }
+    }, 200); // Debounce time in milliseconds
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [settingsMobile, settings, handleResize]);
 
     const renderCard = (index) => {
         const countCodes = (profile && profile.countCodes) || 0;
@@ -61,7 +85,7 @@ export default function SliderCards() {
 
     return (
         <div className="slider-container">
-            <Slider {...settings}>
+            <Slider {...sliderSettings}>
                 {[...Array(10).keys()].map(index => renderCard(index))}
             </Slider>
             {isFullImageOpen && (
